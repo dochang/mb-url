e54124d7d910076dbdf4450074f2aa142d7cf274
Wade Zhang <dochang@gmail.com>
2021-10-27 19:40:27 +0800

    WIP Add test code for `mb-url-http-stderr`

--- test/mb-url-test.el
+++ test/mb-url-test.el
@@ -34,12 +34,16 @@
 (require 'url-http)
 
 (require 's)
+(require 'ert-async)
+(require 'promise)
 
 (require 'mb-url)
 (require 'mb-url-http)
 
 ;; Do not truncate the backtrace.  This makes ERT easy to debug.
 (setq ert-batch-backtrace-right-margin 256)
+
+(setq ert-async-timeout 60)
 
 (setq mb-url-test--httpbin-prefix
       (let ((prefix (getenv "MB_URL_TEST__HTTPBIN_PREFIX")))
@@ -268,6 +272,187 @@ Access-Control-Allow-Credentials: true
                     #'mb-url-http-curl
                     'mb-url-http-httpie
                     #'mb-url-http-httpie)))
+    (advice-remove 'url-http 'mb-url-http-around-advice)))
+
+(defun mb-url-test--url-retrieve-synchronously (url &optional silent inhibit-cookies timeout)
+  "Retrieve URL synchronously.
+Return the buffer containing the data, or nil if there are no data
+associated with it (the case for dired, info, or mailto URLs that need
+no further processing).  URL is either a string or a parsed URL.
+
+If SILENT is non-nil, don't do any messaging while retrieving.
+If INHIBIT-COOKIES is non-nil, refuse to store cookies.  If
+TIMEOUT is passed, it should be a number that says (in seconds)
+how long to wait for a response before giving up."
+  (url-do-setup)
+
+  (let ((retrieval-done nil)
+	(start-time (current-time))
+        (url-asynchronous nil)
+        (asynch-buffer nil))
+    (print "1111111111111111111111111111")
+    (setq asynch-buffer
+	  (url-retrieve url (lambda (&rest ignored)
+			      (url-debug 'retrieval "Synchronous fetching done (%S)" (current-buffer))
+			      (setq retrieval-done t
+				    asynch-buffer (current-buffer)))
+			nil silent inhibit-cookies))
+    (print "222222222222222222222222222222222")
+    (print asynch-buffer)
+    (if (null asynch-buffer)
+        ;; We do not need to do anything, it was a mailto or something
+        ;; similar that takes processing completely outside of the URL
+        ;; package.
+        nil
+      (let ((proc (get-buffer-process asynch-buffer)))
+	;; If the access method was synchronous, `retrieval-done' should
+	;; hopefully already be set to t.  If it is nil, and `proc' is also
+	;; nil, it implies that the async process is not running in
+	;; asynch-buffer.  This happens e.g. for FTP files.  In such a case
+	;; url-file.el should probably set something like a `url-process'
+	;; buffer-local variable so we can find the exact process that we
+	;; should be waiting for.  In the mean time, we'll just wait for any
+	;; process output.
+        (print "333333333333333")
+        (print proc)
+	(while (and (not retrieval-done)
+                    (or (not timeout)
+			(time-less-p (time-since start-time) timeout)))
+          (print "444444444444444444444444444")
+	  (url-debug 'retrieval
+		     "Spinning in url-retrieve-synchronously: %S (%S)"
+		     retrieval-done asynch-buffer)
+          (print "55555555555555555555555555555")
+          (if (buffer-local-value 'url-redirect-buffer asynch-buffer)
+              (setq proc (get-buffer-process
+                          (setq asynch-buffer
+                                (buffer-local-value 'url-redirect-buffer
+                                                    asynch-buffer))))
+            (print "6666666666666666666666")
+            (print (and proc (memq (process-status proc)
+                                   '(closed exit signal failed))
+                        ;; Make sure another process hasn't been started.
+                        (eq proc (or (get-buffer-process asynch-buffer) proc))))
+            (print proc)
+            (when proc
+              (print (memq (process-status proc)
+                           '(closed exit signal failed)))
+              (print (process-status proc))
+              (print (process-command proc))
+              (print (eq proc (or (get-buffer-process asynch-buffer) proc))))
+            (if (and proc (memq (process-status proc)
+                                '(closed exit signal failed))
+                     ;; Make sure another process hasn't been started.
+                     (eq proc (or (get-buffer-process asynch-buffer) proc)))
+                ;; FIXME: It's not clear whether url-retrieve's callback is
+                ;; guaranteed to be called or not.  It seems that url-http
+                ;; decides sometimes consciously not to call it, so it's not
+                ;; clear that it's a bug, but even then we need to decide how
+                ;; url-http can then warn us that the download has completed.
+                ;; In the mean time, we use this here workaround.
+		;; XXX: The callback must always be called.  Any
+		;; exception is a bug that should be fixed, not worked
+		;; around.
+		(progn ;; Call delete-process so we run any sentinel now.
+		  (delete-process proc)
+		  (setq retrieval-done t)))
+            (print "777777777777777777777")
+            ;; We used to use `sit-for' here, but in some cases it wouldn't
+            ;; work because apparently pending keyboard input would always
+            ;; interrupt it before it got a chance to handle process input.
+            ;; `sleep-for' was tried but it lead to other forms of
+            ;; hanging.  --Stef
+            (unless (or (with-local-quit
+			  (accept-process-output proc 1))
+			(null proc))
+              ;; accept-process-output returned nil, maybe because the process
+              ;; exited (and may have been replaced with another).  If we got
+	      ;; a quit, just stop.
+	      (when quit-flag
+		(delete-process proc))
+              (setq proc (and (not quit-flag)
+			      (get-buffer-process asynch-buffer))))
+            (print "888888888888888888"))))
+      (print "99999999999999999")
+      asynch-buffer)))
+
+(ert-deftest-async mb-url-test-053-stderr (done)
+  (unwind-protect
+      (progn
+        (advice-add 'url-http :around 'mb-url-http-around-advice)
+        (promise-chain
+         (promise-all
+          (vconcat
+           (mapcar (lambda (errbuf)
+                     (let* ((mb-url-http-backend
+                             (lambda (name url buffer default-sentinel)
+                               (mb-url-http-make-pipe-process
+                                url name buffer
+                                '("/bin/sh" "-c" "echo \"DEADBEEF\" >&2 ; exit 1")
+                                default-sentinel)))
+                            (url (format "%s/get" mb-url-test--httpbin-prefix))
+                            (mb-url-http-stderr (lambda (url) errbuf))
+                            (begin (with-current-buffer errbuf (buffer-string))))
+                       (promise-new
+                        (lambda (resolve reject)
+                          (url-retrieve url (lambda (&rest args)))
+                          (run-with-idle-timer 5 nil
+                                               (lambda (before)
+                                                 (print "3333333333333333")
+                                                 (with-local-quit
+                                                   (while (accept-process-output (get-buffer-process errbuf))))
+                                                 (let ((after (with-current-buffer errbuf (buffer-string))))
+                                                   (should (string-prefix-p (concat before "DEADBEEF\n") after)))
+                                                 (funcall resolve errbuf))
+                                               before)))))
+                   (list (generate-new-buffer "mb-url-test-053-stderr")))))
+         (then (lambda (&rest args)
+                 (funcall done)))))
+    (advice-remove 'url-http 'mb-url-http-around-advice)))
+
+(ert-deftest mb-url-test-054-stderr ()
+  (unwind-protect
+      (progn
+        (advice-add 'url-http :around 'mb-url-http-around-advice)
+        (mapc (lambda (errbuf)
+                (let* ((mb-url-http-backend
+                        (lambda (name url buffer default-sentinel)
+                          (let ((proc (mb-url-http-make-pipe-process
+                                       url name buffer
+                                       '("/bin/sh" "-c" "sleep 5s ; echo \"DEADBEEF\" >&2 ; exit 1")
+                                       default-sentinel)))
+                            ;; ;; Dump stdout, otherwise emacs will stuck.
+                            ;; ;;
+                            ;; ;; https://www.gnu.org/software/emacs/manual/html_node/elisp/Accepting-Output.html
+                            ;; (while (accept-process-output proc))
+                            ;; (while (accept-process-output (get-buffer-process errbuf)))
+                            (print "###############")
+                            (print (process-status proc))
+                            proc)))
+                       (url (format "%s/get" mb-url-test--httpbin-prefix))
+                       (mb-url-http-stderr (lambda (url) errbuf))
+                       (message-log-max t))
+                  (with-current-buffer errbuf
+                    (let (before after)
+                      (setq before (buffer-string))
+                      ;; (print "2222222222222")
+                      ;; (while (accept-process-output (get-buffer-process errbuf)))
+                      ;; (while (process-live-p (get-buffer-process errbuf))
+                      ;;   (sleep-for 1))
+                      ;; (print "1111111111111")
+                      (with-timeout (60 (error "Timeout"))
+                        (mb-url-test--url-retrieve-synchronously url t t))
+                      ;; (if (>= emacs-major-version 26)
+                      ;;     (url-retrieve-synchronously url t t 60)
+                      ;;   (with-timeout (60 (error "Timeout"))
+                      ;;     (url-retrieve-synchronously url t t)))
+                      ;; (print "333333333333333")
+                      (setq after (buffer-string))
+                      ;; (print "44444444444444")
+                      (should (string-prefix-p (concat before "DEADBEEF\n") after))))))
+              (list (generate-new-buffer "mb-url-test-053-stderr")
+                    ;; (messages-buffer)
+                    )))
     (advice-remove 'url-http 'mb-url-http-around-advice)))
 
 (provide 'mb-url-test)
