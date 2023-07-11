809e331c9f18d50a514970a11ea64d0f1edb9651
Wade Zhang <dochang@gmail.com>
2023-07-11 13:24:08 +0800

    Remove Guix CI jobs

--- manifest.scm
+++
@@ -1,9 +0,0 @@
-;; This "manifest" file can be passed to 'guix package -m' to reproduce
-;; the content of your profile.  This is "symbolic": it only specifies
-;; package names.  To reproduce the exact same profile, you also need to
-;; capture the channels being used, as returned by "guix describe".
-;; See the "Replicating Guix" section in the manual.
-
-(specifications->manifest
- (list "httpie"
-       "curl"))
